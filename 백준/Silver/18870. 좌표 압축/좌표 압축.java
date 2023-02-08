23329ed424b828bc09bdd33267b66f4570a2b305
Bo Ram Kim <68523422+boram0415@users.noreply.github.com>
2023-02-08 17:11:59 +0900

    [Silver II] Title: 좌표 압축, Time: 1816 ms, Memory: 298328 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.java
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -7,16 +7,17 @@ public class Main {
         int[] nums = new int[Integer.parseInt(br.readLine())];
         int w =0;
         StringTokenizer st = new StringTokenizer(br.readLine());
-
+        
+        // 자연수 초기화
         while (st.hasMoreTokens()) {
             nums[w++] = Integer.parseInt(st.nextToken());
         }
-
+        // TreeSet을 이용한 중복제거와 정렬
         Set<Integer> set = new TreeSet<>();
         for (int i = 0; i < nums.length; i++) {
             set.add(nums[i]);
         }
-
+        // 중복제거 정렬된 값 담기
         int[] arr = new int[set.size()];
         Iterator i = set.iterator();
         int j = 0;
@@ -24,7 +25,6 @@ public class Main {
             arr[j++] = Integer.parseInt(i.next().toString());
         }
         //이진탐색
-
         for (int x = 0; x < nums.length; x++) {
             int find = nums[x];
             int mid ;
@@ -45,6 +45,8 @@ public class Main {
 
             }
         }
+
         System.out.println(sb);
+
     }
 }
