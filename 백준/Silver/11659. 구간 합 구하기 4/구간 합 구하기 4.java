ade3d2e2deba6938e8ae94166db36068646ad7c9
Bo Ram Kim <68523422+boram0415@users.noreply.github.com>
2023-02-09 19:55:57 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 612 ms, Memory: 70260 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -6,26 +6,22 @@ public class Main {
         StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine());
         // 줄의 갯수 N
-        int[] arr = new int[Integer.parseInt(st.nextToken())];
+        int[] arr = new int[Integer.parseInt(st.nextToken())+1];
         // 합의 횟수 M
         int cnt = Integer.parseInt(st.nextToken());
         // N개의 수
         st = new StringTokenizer(br.readLine());
 
-        // N개의 수 배열에 담기
-        for (int x = 0; x < arr.length; x++) {
-            arr[x] = Integer.parseInt(st.nextToken());
-        }
-        // 구간 합 구해놓기
+        // N개의 수 구간 합 구하기
         for (int x = 1; x < arr.length; x++) {
-            arr[x] += arr[x - 1];
+            arr[x] = arr[x-1] + Integer.parseInt(st.nextToken());
         }
         for (int x = 0; x < cnt; x++) {
             st = new StringTokenizer(br.readLine());
-            int i = Integer.parseInt(st.nextToken()) - 1;
-            int j = Integer.parseInt(st.nextToken()) - 1;
+            int i = Integer.parseInt(st.nextToken());
+            int j = Integer.parseInt(st.nextToken());
             // index 0이 아닐경우
-            if (i != 0) {
+            if (i != 1) {
                 // arr[i-1]번째 인덱스값을 arr[j]값에서 빼준다.
                 sb.append(arr[j] - arr[i-1] + "\n");
             } else {
