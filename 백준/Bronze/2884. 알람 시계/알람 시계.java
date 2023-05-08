2077d245ccee62368375ad1851389b9306ee9a41
Bo Ram <68523422+boram0415@users.noreply.github.com>
2023-05-08 22:06:17 +0900

    [Bronze III] Title: 알람 시계, Time: 72 ms, Memory: 11516 KB -BaekjoonHub

--- 백준/Bronze/2884. 알람 시계/알람 시계.java
+++ 백준/Bronze/2884. 알람 시계/알람 시계.java
@@ -1,16 +1,21 @@
-import java.util.Scanner;
-
-class Main {
-	public static void main(String[] args) {
-	    Scanner sc = new Scanner(System.in);
-        int h = sc.nextInt();
-        int m = sc.nextInt() - 45 ;
-        if(m < 0 ){
-            m += 60 ;
-            h = h == 0 ? 23 : h-1 ;  
+import java.io.*;
+import java.util.StringTokenizer;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int h = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        String result;
+        // 알람설정 분이 45분보다 클 경우
+        if (m > 45) {
+            result = h + " " + (m - 45);
+            // 작을 경우
+        } else if (m == 45){
+            result = h + " " + 0;
+        } else{
+            result = h != 0 ? --h + " " + ((60 + m) - 45) : 23 + " " + ((60 + m) - 45);
         }
-        System.out.println(h+" "+m);
-	}
-
-	
+        System.out.println(result);
+    }
 }
