ca3239da5f2cb8e483423dee443010258f54ae1a
Bo Ram <68523422+boram0415@users.noreply.github.com>
2023-05-21 18:11:16 +0900

    [Bronze I] Title: 단어 뒤집기, Time: 384 ms, Memory: 42920 KB -BaekjoonHub

--- 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.java
+++ 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.java
@@ -1,24 +1,23 @@
 import java.io.*;
 import java.util.*;
-class Main {
+class Main{
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         int cnt = Integer.parseInt(br.readLine());
         while (cnt > 0) {
             cnt--;
-            String str = br.readLine();
-            StringTokenizer st = new StringTokenizer(str);
+            StringTokenizer st = new StringTokenizer(br.readLine());
             int len = st.countTokens();
             for (int i = 0; i < len; i++) {
-                String temp = st.nextToken();
-                for (int j = temp.length()-1; j >= 0; j--) {
-                    sb.append(temp.charAt(j));
+                String str = st.nextToken();
+                for (int j = str.length() - 1; j >= 0; j--) {
+                    sb.append(str.charAt(j));
                 }
                 sb.append(" ");
             }
             sb.append("\n");
         }
-        System.out.print(sb.toString());
+        System.out.println(sb);
     }
 }
