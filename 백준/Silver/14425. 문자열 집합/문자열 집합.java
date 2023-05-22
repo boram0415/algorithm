5b7401d35f3b50e04834f0539d4cf6696dee5fd4
Bo Ram <68523422+boram0415@users.noreply.github.com>
2023-05-22 17:20:24 +0900

    [Silver III] Title: 문자열 집합, Time: 324 ms, Memory: 38996 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.java
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.java
@@ -1,23 +1,21 @@
 import java.io.*;
 import java.util.*;
+
 public class Main {
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken()), m = Integer.parseInt(st.nextToken());
-        String[] arr = new String[n];
-        // 집합 s에 있는 문자열 담기
+        HashSet<String> set = new HashSet<>();
         for (int i = 0; i < n; i++) {
-            arr[i] = br.readLine();
+            set.add(br.readLine());
         }
-        // 이후 들어오는 문자열과 집합에 있는 문자열 비교 후 카운팅
         int cnt = 0;
         for (int i = 0; i < m; i++) {
-            String str = br.readLine();
-            for (int j = 0; j < n; j++) {
-                if (arr[j].equals(str)) cnt++;
-            }
+            if (set.contains((br.readLine()))) cnt++;
         }
-        System.out.print(cnt);
+        System.out.println(cnt);
+
     }
 }
