f271963347f9bd7dc9bbfd3eb93e079fa313b206
Bo Ram <68523422+boram0415@users.noreply.github.com>
2023-05-28 18:46:41 +0900

    [Bronze I] Title: 피보나치 수, Time: 100 ms, Memory: 13700 KB -BaekjoonHub

--- 백준/Bronze/4150. 피보나치 수/피보나치 수.java
+++ 백준/Bronze/4150. 피보나치 수/피보나치 수.java
@@ -1,22 +1,18 @@
 import java.io.*;
-import java.math.BigInteger;
-
+import java.math.*;
 class Main{
     public static void main(String[] args) throws IOException {
         BufferedReader br  = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         BigInteger[] arr = new BigInteger[n+1];
 
-        arr[0] = BigInteger.valueOf(0);
+        // 피보나치 수열 기본값 세팅
+        arr[0] = BigInteger.valueOf(1);
         arr[1] = BigInteger.valueOf(1);
-
-        if (n > 1) {
-            arr[2] = BigInteger.valueOf(1);
+        // 피보나피 수열 구하기
+        for (int i = 2; i < n; i++) {
+            arr[i] = arr[i-1].add(arr[i-2]);
         }
-
-        for (int i = 3; i <= n; i++) {
-            arr[i]= arr[i - 2].add(arr[i - 1]);
-        }
-        System.out.print(arr[n]);
+        System.out.print(arr[n-1]);
     }
 }
