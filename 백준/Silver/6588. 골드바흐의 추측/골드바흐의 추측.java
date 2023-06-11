6e90997f2520b9cc510bc3cef87983477f709065
Bo Ram <68523422+boram0415@users.noreply.github.com>
2023-06-11 21:20:58 +0900

    [Silver I] Title: 골드바흐의 추측, Time: 504 ms, Memory: 43020 KB -BaekjoonHub

--- 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.java
+++ 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.java
@@ -1,40 +1,43 @@
 import java.io.*;
 import java.util.*;
 
-class Main {
-
-    static int N = 1000001;
-    static boolean [] prime = new boolean[N];
+class Main{
+        static int N = 1000001;
+        static boolean prime[] = new boolean[N];
     public static void main(String[] args) throws IOException {
-
+        
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-
-        for (int i = 2; i < N; i++) {
-            if (!prime[i]) {
-                for (int j = i + i; j < N; j += i) {
-                    prime[j] = true;
+        
+        // 소수 일 경우 false 
+        for(int i=2;i<N;i++){
+            for(int j=i+i;j<N ; j+=i){
+                if(!prime[j]){
+                    prime[j]=true;
                 }
             }
         }
-
-        int n = 0;
-        boolean ch = false;
-        while ((n = Integer.parseInt(br.readLine())) != 0) {
-            for (int i = 2; i < n; i++) {
-                if (!prime[i] && !prime[n-i]) {
-                    sb.append(n + " = " + i + " + " + (n-i) + "\n" );
-                    ch = true;
-                    break;
-                }
-            }
-        }
-
-        if (!ch) {
-            System.out.println("Goldbach's conjecture is wrong.");
+        
+       int n = 0;
+       boolean ch = false;
+       while((n = Integer.parseInt(br.readLine())) != 0){
+           // n = 10 
+           for(int i=2; i<n;i++){
+               if(!prime[i] && !prime[n-i]){
+                   sb.append(n + " = " + i + " + " + (n-i) + "\n");
+                   ch = true;
+                   break;
+               }
+                   
+           }
+       }
+        
+        if(!ch){
+            System.out.print("Goldbach's conjecture is wrong.");
         }else{
-            System.out.println(sb);
+            System.out.print(sb);
         }
-
+        
     }
+    
 }
