c055f011d460531243009b0b342b7339e619ddae
Bo Ram <68523422+boram0415@users.noreply.github.com>
2023-06-12 18:42:47 +0900

    [Silver I] Title: 소수&팰린드롬, Time: 144 ms, Memory: 14060 KB -BaekjoonHub

---
+++ 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+
+    static int N = 1000000 * 2;
+    static boolean palindrome(int n) {
+        int cp = n,rev = 0;
+        while (cp != 0) {
+            int tmp = cp % 10;
+            rev = rev * 10 + tmp;
+            cp/=10;
+        }
+        return n == rev ? true : false;
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        boolean[] isPrime = new boolean[N + 1];
+        isPrime[0] = isPrime[1] = true;
+
+        for (int i = 2; i < (int) Math.sqrt(N); i++) {
+            for (int j = i * i; j <= N; j += i) {
+                isPrime[j] = true;
+            }
+        }
+
+        int t = Integer.parseInt(br.readLine());
+        int tmp = t;
+        for (int i = tmp; i <= N; i++) {
+            if (!isPrime[i]) {
+                tmp = i;
+                if (palindrome(tmp)) {
+                    System.out.println(tmp);
+                    break;
+                }
+            }
+            tmp++;
+        }
+    }
+}
