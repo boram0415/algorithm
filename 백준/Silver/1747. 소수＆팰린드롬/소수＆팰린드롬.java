423e1ca6a2fc8396f75cfafda59c2760aecbc119
Bo Ram <68523422+boram0415@users.noreply.github.com>
2023-06-12 19:09:44 +0900

    [Silver I] Title: 소수&팰린드롬, Time: 124 ms, Memory: 12880 KB -BaekjoonHub

--- 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.java
+++ 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.java
@@ -1,43 +1,37 @@
 import java.io.*;
 import java.util.*;
-
-class Main {
-
+class Main{
+    
     static int N = 1003001;
-    static boolean palindrome(int n) {
-        int cp = n,rev = 0;
-        while (cp != 0) {
-            int tmp = cp % 10;
-            rev = rev * 10 + tmp;
-            cp/=10;
+    static boolean [] isPrime = new boolean[N+1];
+    
+    static boolean palindrome(int n){
+        int cp = n , tmp =0,rev=0;
+        while(cp != 0){
+            tmp = cp % 10;
+            rev = rev * 10 + tmp ;
+            cp /= 10;
         }
-        return n == rev ? true : false;
+        return n == rev ? true : false ;
     }
-
-    public static void main(String[] args) throws IOException {
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        boolean[] isPrime = new boolean[N + 1];
-        isPrime[0] = isPrime[1] = true;
-
-        for (int i = 2; i < (int) Math.sqrt(N); i++) {
-            for (int j = i * i; j <= N; j += i) {
-                isPrime[j] = true;
+    
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
+        // 소수 판별
+        for(int i=2;i<=(int)Math.sqrt(N);i++){
+            for(int j=i*i; j<=N;j+=i){
+                isPrime[j]=true;
             }
         }
-
+        isPrime[1]=true;
         int t = Integer.parseInt(br.readLine());
-        int tmp = t;
-        for (int i = tmp; i <= N; i++) {
-            if (!isPrime[i]) {
-                tmp = i;
-                if (palindrome(tmp)) {
-                    System.out.println(tmp);
+        for(int i = t; t < N ; i++){
+            if(!isPrime[i]){
+                if(palindrome(i)){
+                    System.out.print(i);
                     break;
                 }
             }
-            tmp++;
         }
     }
 }
