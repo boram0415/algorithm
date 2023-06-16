9013e998a8dcac2fdfb7fa56719b2f2ced40237a
Bo Ram <68523422+boram0415@users.noreply.github.com>
2023-06-16 19:09:16 +0900

    [Gold V] Title: 소수인팰린드롬, Time: 1704 ms, Memory: 386216 KB -BaekjoonHub

--- 백준/Gold/1990. 소수인팰린드롬/소수인팰린드롬.java
+++ 백준/Gold/1990. 소수인팰린드롬/소수인팰린드롬.java
@@ -3,8 +3,13 @@ import java.util.*;
 
 class Main {
     static boolean isPrime[];
+    static int[] check = {1, 3, 7, 9};
     static boolean palindrome(int n) {
         String t = n + "";
+        if (n == 5) return true;
+        if (Arrays.binarySearch(check,t.charAt(0)-'0') < -1) {
+            return false;
+        }
         for (int i = 0; i < t.length() / 2; i++) {
             if (t.charAt(i) != t.charAt(t.length() - i - 1)) {
                 return false;
@@ -12,20 +17,23 @@ class Main {
         }
         return true;
     }
+
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         StringBuilder sb = new StringBuilder();
         int a = Integer.parseInt(st.nextToken());
         int b = Integer.parseInt(st.nextToken());
         isPrime = new boolean[b + 1];
-        for (int i = 2; i<=(int)Math.sqrt(b); i++) {
+        for (int i = 2; i <= (int) Math.sqrt(b); i++) {
             if (!isPrime[i]) {
-                for (int j = i*i; j <= b; j += i) {
+                for (int j = i * i; j <= b; j += i) {
                     isPrime[j] = true;
                 }
             }
         }
+
         for (int i = a; i <= b; i++) {
             if (!isPrime[i] && palindrome(i)) {
                 sb.append(i).append("\n");
@@ -33,5 +41,6 @@ class Main {
         }
         sb.append(-1);
         System.out.println(sb);
+
     }
 }
