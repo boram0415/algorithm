59a76f8295f6cad8189a7ff99d375ca95a8626f5
Bo Ram <68523422+boram0415@users.noreply.github.com>
2023-06-17 00:35:49 +0900

    [Gold V] Title: 소수인팰린드롬, Time: 232 ms, Memory: 22440 KB -BaekjoonHub

--- 백준/Gold/1990. 소수인팰린드롬/소수인팰린드롬.java
+++ 백준/Gold/1990. 소수인팰린드롬/소수인팰린드롬.java
@@ -1,37 +1,44 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-class Main{
+public class Main {
+    // 팰린드롬이면서 소수인 마지막 수 
+    static int N = 9989899;
     static boolean isPrime[];
-    static boolean pal(int n){
-        int result=0,cp=n,tmp=0;
-        while (cp !=0) {
-            tmp = cp%10;
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int min = Math.min(Integer.parseInt(st.nextToken()), N);
+        isPrime = new boolean[min + 1];
+        
+        for (int i = 2; i <= (int) Math.sqrt(min); i++) {
+            if (isPrime[i]) continue;
+            for (int j = i * i; j <= min; j += i) {
+                if (!isPrime[j]) isPrime[j] = true;
+            }
+        }
+        
+        for (int i = a; i <= min; i++) {
+            if (!isPrime[i]) {
+                if (pal(i)) bw.write(i + "\n");
+            }
+        }
+        bw.write("-1");
+        bw.flush();
+        bw.close();
+    }
+
+    static boolean pal(int n) {
+        int result = 0, cp = n;
+        while (cp != 0) {
+            int tmp = cp % 10;
             result = result * 10 + tmp;
             cp /= 10;
         }
         return result == n;
     }
-    public static void main(String[] args) throws IOException{
-        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        StringBuilder sb = new StringBuilder();
-        int a = Integer.parseInt(st.nextToken());
-        int b = Integer.parseInt(st.nextToken());
-        isPrime = new boolean[b+1];
-        //에토체 소수 구하기
-        for (int i = 2; i <= (int) Math.sqrt(b); i++) {
-            if(isPrime[i]) continue;
-            for (int j = i * i; j <= b; j += i) {
-                if (!isPrime[j]) isPrime[j] = true;
-            }
-        }
-        for(int i=a;i<=b;i++){
-            if(!isPrime[i] && pal(i)){
-                sb.append(i).append("\n");
-            }
-        }
-        sb.append(-1);
-        System.out.print(sb);
-    }
+
 }
