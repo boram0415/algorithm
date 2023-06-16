34470e030992472d5daf015d628fd0343d80e9b3
Bo Ram <68523422+boram0415@users.noreply.github.com>
2023-06-16 20:27:44 +0900

    [Gold V] Title: 소수인팰린드롬, Time: 1500 ms, Memory: 109500 KB -BaekjoonHub

--- 백준/Gold/1990. 소수인팰린드롬/소수인팰린드롬.java
+++ 백준/Gold/1990. 소수인팰린드롬/소수인팰린드롬.java
@@ -1,46 +1,37 @@
 import java.io.*;
 import java.util.*;
 
-class Main {
+class Main{
     static boolean isPrime[];
-    static int[] check = {1, 3, 7, 9};
-    static boolean palindrome(int n) {
-        String t = n + "";
-        if (n == 5) return true;
-        if (Arrays.binarySearch(check,t.charAt(0)-'0') < -1) {
-            return false;
+    static boolean pal(int n){
+        int result=0,cp=n,tmp=0;
+        while (cp !=0) {
+            tmp = cp%10;
+            result = result * 10 + tmp;
+            cp /= 10;
         }
-        for (int i = 0; i < t.length() / 2; i++) {
-            if (t.charAt(i) != t.charAt(t.length() - i - 1)) {
-                return false;
-            }
-        }
-        return true;
+        return result == n;
     }
-
-    public static void main(String[] args) throws IOException {
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException{
+        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         StringBuilder sb = new StringBuilder();
         int a = Integer.parseInt(st.nextToken());
         int b = Integer.parseInt(st.nextToken());
-        isPrime = new boolean[b + 1];
+        isPrime = new boolean[b+1];
+        //에토체 소수 구하기
         for (int i = 2; i <= (int) Math.sqrt(b); i++) {
-            if (!isPrime[i]) {
-                for (int j = i * i; j <= b; j += i) {
-                    isPrime[j] = true;
-                }
+            if(isPrime[i]) continue;
+            for (int j = i * i; j <= b; j += i) {
+                if (!isPrime[j]) isPrime[j] = true;
             }
         }
-
-        for (int i = a; i <= b; i++) {
-            if (!isPrime[i] && palindrome(i)) {
+        for(int i=a;i<=b;i++){
+            if(!isPrime[i] && pal(i)){
                 sb.append(i).append("\n");
             }
         }
         sb.append(-1);
-        System.out.println(sb);
-
+        System.out.print(sb);
     }
 }
