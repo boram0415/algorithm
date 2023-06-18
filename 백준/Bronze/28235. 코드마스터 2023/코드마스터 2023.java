10b5026b07b84dd45adf133c41b79bbf1e412b55
Bo Ram <68523422+boram0415@users.noreply.github.com>
2023-06-19 02:59:12 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 76 ms, Memory: 11476 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        switch (str) {
+            case "SONGDO":
+                System.out.println("HIGHSCHOOL");
+                break;
+            case "CODE":
+                System.out.println("MASTER");
+                break;
+            case "2023":
+                System.out.println("0611");
+                break;
+            case "ALGORITHM":
+                System.out.println("CONTEST");
+                break;
+        }
+    }
+}
