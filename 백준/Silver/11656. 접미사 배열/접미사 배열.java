55dff8a4b337c7c7d0054a382f7fb9c5536f5134
Bo Ram <68523422+boram0415@users.noreply.github.com>
2023-06-18 18:55:54 +0900

    [Silver IV] Title: 접미사 배열, Time: 104 ms, Memory: 17660 KB -BaekjoonHub

--- 백준/Silver/11656. 접미사 배열/접미사 배열.java
+++ 백준/Silver/11656. 접미사 배열/접미사 배열.java
@@ -5,23 +5,13 @@ public class Main {
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String S = br.readLine();
-        TreeSet<String> set = new TreeSet<>();
-        String tmp = S;
-        for (int j = 0; j < S.length(); j++) {
-            for (char i = 'a'; i <= 'z'; i++) {
-                if (S.charAt(j) == i) {
-                    set.add(tmp.substring(j));
-                    break;
-                }
-            }
-        }
-        set.add(S.charAt(S.length()-1)+"");
-
         StringBuilder sb = new StringBuilder();
-        Iterator i = set.iterator();
-        while (i.hasNext()) {
-            sb.append(i.next()).append("\n");
+        String[] str = new String[S.length()];
+        for (int i = 0; i < S.length(); i++) {
+            str[i] = S.substring(i);
         }
+        Arrays.sort(str);
+        for(String i : str) sb.append(i).append("\n");
         System.out.println(sb);
     }
 }
