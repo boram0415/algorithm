496841438628f7b1e0e1050757f4f2105d64ee40
Bo Ram <68523422+boram0415@users.noreply.github.com>
2023-06-19 00:41:09 +0900

    [Silver II] Title: 창고 다각형, Time: 88 ms, Memory: 11972 KB -BaekjoonHub

--- 백준/Silver/2304. 창고 다각형/창고 다각형.java
+++ 백준/Silver/2304. 창고 다각형/창고 다각형.java
@@ -1,50 +1,40 @@
 import java.util.*;
 import java.io.*;
-
 public class Main {
-    static int sum;
-    static int idx=0;
-
-    static void func(int hei, int wid ,int priorWid, int heightMax) {
-        int x = wid - priorWid;
-        int y = heightMax-hei;
-        sum += (x*y);
-    }
-
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-        int t = Integer.parseInt(br.readLine());
-        int n[][] = new int[t][2];
-        int heightMax = 0;
-        for (int i = 0; i < n.length; i++) {
+        int T = Integer.parseInt(br.readLine());
+        int arr[] = new int[1001];
+        int result = 0 , idx=0;
+        int maxHeight = 0, maxWidth = 0, minWidth = 1001;
+        for (int i = 0; i < T; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < 2; j++) {
-                n[i][j] = Integer.parseInt(st.nextToken());
+            int L = Integer.parseInt(st.nextToken());
+            int H = Integer.parseInt(st.nextToken());
+            arr[L] = H;
+            if (maxHeight < H) {
+                maxHeight = H;
+                idx = L;
             }
-            heightMax = Math.max(heightMax, n[i][1]);
+            maxHeight = Math.max(maxHeight, H);
+            maxWidth = Math.max(maxWidth, L);
+            minWidth = Math.min(minWidth, L);
         }
-        Arrays.sort(n, (o1, o2) -> {
-            return o1[0] - o2[0];
-        });
-
-        int area = (n[n.length - 1][0] + 1 - n[0][0]) * heightMax;
-        for (int i = 1; i < n.length; i++) {
-            if (n[idx][1] <= n[i][1]) {
-                func(n[idx][1],n[i][0],n[idx][0], heightMax);
-                idx = i;
+        int tmp = 0;
+        for (int i = minWidth; i <= idx; i++) {
+            if (tmp < arr[i]) {
+                tmp = arr[i];
             }
+            result += tmp;
         }
-        int tmp =idx,len = n.length-1;
-        if (idx != len) {
-            idx = len;
-            for (int j = len-1; j >= tmp ; j--) {
-                if (n[idx][1] <= n[j][1]) {
-                    func(n[idx][1],n[idx][0],n[j][0],heightMax);
-                    idx = j;
-                }
+        tmp=0;
+        for (int i = maxWidth; i > idx; i--) {
+            if (tmp < arr[i]) {
+                tmp = arr[i];
             }
+            result += tmp;
         }
-        System.out.println(area-sum);
+        System.out.println(result);
     }
 }
