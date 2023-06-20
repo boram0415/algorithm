4eb2133695a92ee2df0b7b3306efa52b150663dd
Bo Ram <68523422+boram0415@users.noreply.github.com>
2023-06-20 14:19:05 +0900

    [Bronze II] Title: 분해합, Time: 76 ms, Memory: 11440 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.java
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -1,24 +1,31 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Main {
-    public static void main(String args[]) throws IOException {
-        int n = Integer.parseInt(new BufferedReader(new InputStreamReader(System.in)).readLine());
-        for (int i = 100; i >= 0; i--) {
-            if (sum(n-i) == n) {
-                System.out.println((n-i));
-                return;
-            }
-        }
-        System.out.println(0);
-    }
-
-    static int sum(int con) {
-        int res = 0, n =con;
-        while (n != 0) {
-            res += n % 10;
-            n /= 10;
-        }
-        return con+res;
-    }
-
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String autok = br.readLine();
+		int c = Integer.valueOf(autok);
+		
+		int i = c - String.valueOf(c).length() * 9;
+		int m = 0;
+		
+		while(true) {
+			i++;
+			
+			char[] c_arr = String.valueOf(i).toCharArray() ;
+			int sum = i;
+			
+			for(int ca=0;ca < c_arr.length;ca++) {
+				sum += (c_arr[ca]-48);
+			}
+			m = sum;
+			if(sum == c || i >= c) {
+				break;
+			}
+		}
+		
+		if(m != c) i = 0;
+		System.out.println(i);
+	}
 }
