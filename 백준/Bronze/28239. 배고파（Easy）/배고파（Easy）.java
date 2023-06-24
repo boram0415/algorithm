244a5cdd674a3c98f280909acb48b34b41f71277
Bo Ram <68523422+boram0415@users.noreply.github.com>
2023-06-24 16:44:21 +0900

    [Bronze I] Title: 배고파(Easy), Time: 176 ms, Memory: 18004 KB -BaekjoonHub

--- 백준/Bronze/28239. 배고파（Easy）/배고파（Easy）.java
+++ 백준/Bronze/28239. 배고파（Easy）/배고파（Easy）.java
@@ -1,24 +1,24 @@
 import java.io.*;
-import java.io.*;
-class Main{
-    public static void main(String[]args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        int n = Integer.parseInt(br.readLine());
-        while(n-- > 0){
-            long m = Long.parseLong(br.readLine()); // 10
-            boolean flag = false;
-            for(int i=0;i<=63;i++){
-                for(int j=63;j>=0;j--){
-                    if((long)Math.pow(2,i) +(long)Math.pow(2,j) == m){
-                        flag = true;
-                        sb.append(i).append(" ").append(j).append("\n");
-                        break;
-                    }
+class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws Exception {
+        int N = Integer.parseInt(br.readLine());
+        for(int i=0;i<N;i++){
+            long now = Long.parseLong(br.readLine());
+            if(Long.bitCount(now)==2) {
+                for (int b = 0; b < 64; b++) {
+                    if ((now & (1L << b)) != 0) sb.append(b).append(' ');
                 }
-                if(flag) break;
+                sb.append('\n');
+            }else{
+                for (int b = 0; b < 64; b++) {
+                    if ((now & (1L << b)) != 0) sb.append(b-1).append(' ').append(b-1).append('\n');
+                }
             }
         }
-        System.out.print(sb);
+        bw.write(sb.toString());
+        bw.flush();
     }
 }
