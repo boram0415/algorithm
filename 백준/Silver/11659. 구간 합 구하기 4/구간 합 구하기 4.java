15f016607fab4ddb3074d5f95e3e228bc12544d6
Bo Ram <68523422+boram0415@users.noreply.github.com>
2023-06-26 00:50:42 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 584 ms, Memory: 59700 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,33 +1,34 @@
 import java.io.*;
 import java.util.*;
+
 public class Main {
-    public static void main(String[] args) throws IOException {
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws Exception {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        // 줄의 갯수 N
-        int[] arr = new int[Integer.parseInt(st.nextToken())+1];
-        // 합의 횟수 M
-        int cnt = Integer.parseInt(st.nextToken());
-        // N개의 수
+        st = new StringTokenizer(br.readLine());
+        int[] arr = new int[Integer.parseInt(st.nextToken())];
+        int M = Integer.parseInt(st.nextToken());
         st = new StringTokenizer(br.readLine());
 
-        // N개의 수 구간 합 구하기
-        for (int x = 1; x < arr.length; x++) {
-            arr[x] = arr[x-1] + Integer.parseInt(st.nextToken());
+        arr[0] = Integer.parseInt(st.nextToken());
+        for (int i = 1; i < arr.length; i++) {
+            arr[i] = arr[i-1] + Integer.parseInt(st.nextToken());
         }
-        for (int x = 0; x < cnt; x++) {
+
+        while (M-- > 0) {
             st = new StringTokenizer(br.readLine());
-            int i = Integer.parseInt(st.nextToken());
-            int j = Integer.parseInt(st.nextToken());
-            // index 0이 아닐경우
-            if (i != 1) {
-                // arr[i-1]번째 인덱스값을 arr[j]값에서 빼준다.
-                sb.append(arr[j] - arr[i-1] + "\n");
-            } else {
-                sb.append(arr[j] + "\n");
+            int i = Integer.parseInt(st.nextToken())-1;
+            int j = Integer.parseInt(st.nextToken())-1;
+            if(i==0) {
+                sb.append(arr[j]).append("\n");
+                continue;
             }
+            sb.append(arr[j]-arr[i-1]).append("\n");
         }
         System.out.println(sb);
+        // 5 9 12 14 15
     }
 }
