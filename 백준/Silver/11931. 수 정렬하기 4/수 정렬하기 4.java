7996e2e3ab40821a921f3494beca8c9012aa6558
Bo Ram <68523422+boram0415@users.noreply.github.com>
2023-07-09 02:29:56 +0900

    [Silver V] Title: 수 정렬하기 4, Time: 1760 ms, Memory: 222276 KB -BaekjoonHub

--- 백준/Silver/11931. 수 정렬하기 4/수 정렬하기 4.java
+++ 백준/Silver/11931. 수 정렬하기 4/수 정렬하기 4.java
@@ -1,19 +1,19 @@
 import java.io.*;
 import java.util.*;
 
-public class Main{
-	public static void main(String[] args) throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+public class Main {
+    public static void main(String[] args) throws Exception{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken());
-		Integer arr[] = new Integer[n];
-		for (int i = 0; i < n; i++) {
-			arr[i] = Integer.parseInt(br.readLine());
-		}
-		Arrays.sort(arr, Collections.reverseOrder());
-		for (Integer i : arr) 
-			sb.append(i).append("\n");
-		System.out.print(sb);
-	}
+        PriorityQueue<Integer> priQ = new PriorityQueue<>(Collections.reverseOrder());
+
+        int size = Integer.parseInt(br.readLine());
+        for(int i = 0; i < size; i++) {
+            priQ.add(Integer.parseInt(br.readLine()));
+        }
+        while(!priQ.isEmpty()) sb.append(priQ.poll()).append("\n");
+        System.out.println(sb.toString().trim());
+    }
+
 }
