44900810f74ea57f6aec0e1f00949bf03dc2f057
Bo Ram <68523422+boram0415@users.noreply.github.com>
2023-07-08 15:48:33 +0900

    [Bronze III] Title: 세 수, Time: 76 ms, Memory: 11508 KB -BaekjoonHub

--- 백준/Bronze/10817. 세 수/세 수.java
+++ 백준/Bronze/10817. 세 수/세 수.java
@@ -1,36 +1,14 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
-public class Main {
-    static int []arr ;
-    public static void main(String[] args) throws Exception{
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        arr = new int[3];
-        for (int i = 0; i < 3; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-        // 퀵 정렬 sort
-        quickSort(arr,0,arr.length-1);
-        System.out.println(arr[1]);
-    }
-
-    private static void quickSort(int[] arr , int start,int end) {
-        if (start < end) {
-            int part = partition(arr, start, end);
-            quickSort(arr,start,part-1);
-            quickSort(arr,part+1,end);
-        }
-    }
-
-    private static int partition(int[] arr, int start, int end) {
+class Main{
+    static int []arr;
+    
+    static int partition(int[] arr , int start,int end){
         int pivot = arr[end];
         int idx = start-1;
         for(int i=start;i<end;i++){
-            if(arr[i] <= pivot){
+            if(arr[i]<=pivot){
                 idx++;
                 swap(idx,i);
             }
@@ -38,11 +16,32 @@ public class Main {
         swap(idx+1,end);
         return idx+1;
     }
-    private static void swap(int l,int r){
-        int tmp = arr[r];
-        arr[r] = arr[l];
-        arr[l] = tmp;
+    
+    static void quickSort(int[] arr , int start,int end){
+        if(start<end){
+            int part = partition(arr,start,end);
+            quickSort(arr,start,part-1);
+            quickSort(arr,part+1,end);
+        }
     }
-
-
+    
+    static void swap(int l,int r){
+        int tmp=arr[r];
+        arr[r]=arr[l];
+        arr[l]=tmp;
+    }
+    
+    
+    public static void main(String[]args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        arr = new int[3];
+        for(int i=0;i<3;i++){
+            arr[i]=Integer.parseInt(st.nextToken());
+        }
+        //퀵 정렬
+        quickSort(arr,0,arr.length-1);
+        System.out.print(arr[1]);
+    }
+    
 }
