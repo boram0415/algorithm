cf6f9a58815473e53eefc188a480f716734a7642
Bo Ram <68523422+boram0415@users.noreply.github.com>
2023-07-09 02:42:36 +0900

    [Silver V] Title: 수 정렬하기 5, Time: 25532 ms, Memory: 221424 KB -BaekjoonHub

---
+++ 백준/Silver/15688. 수 정렬하기 5/수 정렬하기 5.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        PriorityQueue<Integer> priQ = new PriorityQueue<>();
+
+        int size = Integer.parseInt(br.readLine());
+        for(int i = 0; i < size; i++) {
+            priQ.add(Integer.parseInt(br.readLine()));
+        }
+        while(!priQ.isEmpty()) sb.append(priQ.poll()).append("\n");
+        System.out.println(sb.toString().trim());
+    }
+
+}
