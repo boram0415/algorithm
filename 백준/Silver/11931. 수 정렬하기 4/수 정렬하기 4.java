366f6cc88eb6380dc7dce7b3f76f4b4eb7680f8b
Bo Ram <68523422+boram0415@users.noreply.github.com>
2023-07-08 22:53:46 +0900

    [Silver V] Title: 수 정렬하기 4, Time: 2672 ms, Memory: 215812 KB -BaekjoonHub

--- 백준/Silver/11931. 수 정렬하기 4/수 정렬하기 4.java
+++ 백준/Silver/11931. 수 정렬하기 4/수 정렬하기 4.java
@@ -1,27 +1,19 @@
-
 import java.io.*;
 import java.util.*;
 
 public class Main{
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
-
-	
+        StringBuilder sb = new StringBuilder();
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int n = Integer.parseInt(st.nextToken());
 		Integer arr[] = new Integer[n];
 		for (int i = 0; i < n; i++) {
 			arr[i] = Integer.parseInt(br.readLine());
 		}
-		
-		
 		Arrays.sort(arr, Collections.reverseOrder());
-		
 		for (Integer i : arr) 
-			writer.write(i+"\n"); 
-		writer.flush();
-
-		
+			sb.append(i).append("\n");
+		System.out.print(sb);
 	}
 }
