dea1946af2fa2222a6682b5c07fe311d59b3e5f6
Bo Ram <68523422+boram0415@users.noreply.github.com>
2023-07-12 03:44:53 +0900

    [Silver II] Title: DFS와 BFS, Time: 196 ms, Memory: 17668 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -3,21 +3,21 @@ import java.util.*;
 
 public class Main {
     static boolean visited[];
+    static int N;
     static ArrayList<Integer> A[];
+    static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
         int V = Integer.parseInt(st.nextToken());
         A = new ArrayList[N+1];
         visited = new boolean[N+1];
-        // 리스트 초기화
+        
         for (int i =1; i <=N; i++) {
             A[i] = new ArrayList<>();
         }
-//        System.out.println(Arrays.toString(A));
-        // 값 세팅
         for (int i = 0; i <M; i++) {
             st = new StringTokenizer(br.readLine());
             int p = Integer.parseInt(st.nextToken());
@@ -25,25 +25,22 @@ public class Main {
             A[p].add(c);
             A[c].add(p);
         }
-//        System.out.println(Arrays.toString(A));
-        // 정렬
         for (int i = 1; i <=N; i++) {
             Collections.sort(A[i]);
         }
-//        System.out.println(Arrays.toString(A));
         dfs(V);
-        System.out.println();
-        visited = new boolean[N+1];
+        sb.append("\n");
         bfs(V);
+        System.out.println(sb);
     }
-    // queue 구현
     private static void bfs(int node) {
         Queue<Integer> q = new LinkedList<>();
         q.add(node);
+        visited = new boolean[N+1];
         visited[node] = true;
         while (!q.isEmpty()) {
             int newNode=q.poll();
-            System.out.print(newNode+" ");
+            sb.append(newNode).append(" ");
             for (int i : A[newNode]) {
                 if (!visited[i]) {
                     visited[i] = true;
@@ -52,9 +49,8 @@ public class Main {
             }
         }
     }
-    // 재귀 구현
     private static void dfs(int node) {
-        System.out.print(node+" ");
+        sb.append(node).append(" ");
         visited[node] = true;
         for (int i : A[node]) {
             if(!visited[i]) dfs(i);
