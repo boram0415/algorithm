94e9b5ee46fc8143450a1a1d9776efda17f1afae
Bo Ram <68523422+boram0415@users.noreply.github.com>
2023-07-17 17:28:27 +0900

    [Silver IV] Title: 주몽, Time: 100 ms, Memory: 12892 KB -BaekjoonHub

---
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+
+        int N = read();
+        int M = read();
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = read();
+        }
+        Arrays.sort(arr);
+        int s =0,e=N-1,cnt=0;
+        while(s < e){
+            if (arr[s]+arr[e] > M) {
+                e--;
+            } else if(arr[s]+arr[e] < M){
+                s++;
+            }else{
+                cnt++;
+                e--;
+                s++;
+            }
+        }
+        System.out.println(cnt);
+    }
+    static int read() throws Exception {
+        int c, n = 0;
+        while (true) {
+            c = System.in.read() - 48;
+            if (c < 0 || c > 9) return n;
+            n = n * 10 + c;
+        }
+    }
+}
