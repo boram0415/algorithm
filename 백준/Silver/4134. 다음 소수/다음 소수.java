11c4e827af78edbd9c4aba3a35ce81f21cf511e3
Bo Ram <68523422+boram0415@users.noreply.github.com>
2023-07-19 02:17:11 +0900

    [Silver IV] Title: 다음 소수, Time: 208 ms, Memory: 12664 KB -BaekjoonHub

--- 백준/Silver/4134. 다음 소수/다음 소수.java
+++ 백준/Silver/4134. 다음 소수/다음 소수.java
@@ -1,66 +1,58 @@
 import java.io.*;
+import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    static long read() throws Exception {
+        long c, m = 0;
+        while (true) {
+            c = System.in.read() - 48;
+            if (c < 0 || c > 9) return m;
+            m = m * 10 + c;
+        }
+    }
 
+    public static void main(String[] args) throws Exception {
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
         final int ARR_SIZE = 63300;
         int[] primeNum = new int[ARR_SIZE];
-
         int T;
         long n;
         boolean isPrime = false;
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        for (int i = 0; i < ARR_SIZE; i++) {
-            primeNum[i] = 1;
-        }
-        primeNum[0] = 0;
-        primeNum[1] = 0;
-
+        primeNum[0] = 1;
+        primeNum[1] = 1;
         for (int i = 2; i < ARR_SIZE; i++) {
             for (int j = 2; i * j < ARR_SIZE; j++) {
-
-                primeNum[i * j] = 0;
+                primeNum[i * j] = 1;
             }
         }
 
-        T = Integer.parseInt(br.readLine());
-
-        for (int t = 0; t < T; t++) {
-
-            n = Long.parseLong(br.readLine());
-
+        T = (int)read();
+        while (T-- > 0) {
+            n = read();
             if (n <= 1) {
-
-                System.out.println(2);
+                sb.append("2").append("\n");
             } else {
-
-
-                for (long i = n;; i++) {
-
+                for (long i = n; ; i++) {
                     int m = (int) Math.sqrt(i);
                     isPrime = true;
-
                     for (int j = 1; j <= m; j++) {
-
-                        if (primeNum[j] == 1) {
-
+                        if (primeNum[j] == 0) {
                             if (i % j == 0) {
-
                                 isPrime = false;
                                 break;
                             }
                         }
                     }
-
                     if (isPrime) {
-                        System.out.println(i);
+                        sb.append(i).append("\n");
                         break;
                     }
                 }
             }
-
-        }
+        } // while end
+        System.out.println(sb.toString().trim());
     }
 }
