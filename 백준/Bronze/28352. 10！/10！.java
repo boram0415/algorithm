499bbc8fdc43f0f5cf249efedc73b8c40ad7e674
Bo Ram <68523422+boram0415@users.noreply.github.com>
2023-07-20 19:28:41 +0900

    [Bronze IV] Title: 10!, Time: 76 ms, Memory: 11340 KB -BaekjoonHub

--- 백준/Bronze/28352. 10！/10！.java
+++ 백준/Bronze/28352. 10！/10！.java
@@ -1,27 +1,19 @@
+import java.io.*;
+
 public class Main {
-    static int read()throws Exception{
-        int c,n=0;
+     private static int read() throws Exception {
+        int c, n = 0;
         while (true) {
             c = System.in.read()-48;
-            if( c < 0 || c > 9 ) return n;
+            if (c < 0 || c > 9) return n;
             n = n * 10 + c;
         }
     }
-
-    public static void main(String[] args) throws Exception{
-
-        int week = 7*24*60*60;
-
+    public static void main(String[] args) throws Exception {
         int N = read();
-        long sum =1;
-        for (int i = 1; i <= N; i++) {
-            sum *=i;
-        }
-        for (long i = 1; i <= 588107520; i++) {
-            if (week*i == sum) {
-                System.out.println(i);
-            }
-        }
-
+        long ans = 6;
+        for (int i = 11; i <= N; i++)
+            ans *= i;
+        System.out.println(ans);
     }
 }
