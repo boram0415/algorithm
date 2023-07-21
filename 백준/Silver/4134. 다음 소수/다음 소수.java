f0971421bdda1f34c4d2ab91e53fb30d64a792c8
Bo Ram <68523422+boram0415@users.noreply.github.com>
2023-07-21 19:56:05 +0900

    [Silver IV] Title: 다음 소수, Time: 188 ms, Memory: 12296 KB -BaekjoonHub

--- 백준/Silver/4134. 다음 소수/다음 소수.java
+++ 백준/Silver/4134. 다음 소수/다음 소수.java
@@ -2,34 +2,27 @@ import java.io.*;
 import java.util.Arrays;
 
 public class Main {
-    static long read() throws Exception {
-        long c, m = 0;
-        while (true) {
-            c = System.in.read() - 48;
-            if (c < 0 || c > 9) return m;
-            m = m * 10 + c;
-        }
-    }
-
     public static void main(String[] args) throws Exception {
 
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
-        final int ARR_SIZE = 63246;
-        int[] primeNum = new int[ARR_SIZE];
+        int arr = 63300;
+        int[] primeNum = new int[arr];
         int T;
         long n;
         boolean isPrime = false;
 
         primeNum[0] = 1;
         primeNum[1] = 1;
-        for (int i = 2; i < ARR_SIZE; i++) {
-            for (int j = 2; i * j < ARR_SIZE; j++) {
-                primeNum[i * j] = 1;
+
+        for (int i = 2; i <=Math.sqrt(arr); i++) {
+            if(primeNum[i] == 1) continue;
+            for (int j = i*i; j < arr; j+=i) {
+                primeNum[j] = 1;
             }
         }
 
-        T = (int)read();
+        T = (int) read();
         while (T-- > 0) {
             n = read();
             if (n <= 1) {
@@ -38,12 +31,10 @@ public class Main {
                 for (long i = n; ; i++) {
                     int m = (int) Math.sqrt(i);
                     isPrime = true;
-                    for (int j = 1; j <= m; j++) {
-                        if (primeNum[j] == 0) {
-                            if (i % j == 0) {
-                                isPrime = false;
-                                break;
-                            }
+                    for (int j = 2; j <= m; j++) {
+                        if (primeNum[j] == 0 && i % j == 0) {
+                            isPrime = false;
+                            break;
                         }
                     }
                     if (isPrime) {
@@ -52,7 +43,16 @@ public class Main {
                     }
                 }
             }
-        } // while end
+        }// while end
         System.out.println(sb.toString().trim());
+    }
+
+    static long read() throws Exception {
+        long c, m = 0;
+        while (true) {
+            c = System.in.read() - 48;
+            if (c < 0 || c > 9) return m;
+            m = m * 10 + c;
+        }
     }
 }
