63f86cf9cc14424b685661b049b801c0b047ea07
Bo Ram <68523422+boram0415@users.noreply.github.com>
2023-07-21 17:08:03 +0900

    [Bronze I] Title: 유니대전 퀴즈쇼, Time: 88 ms, Memory: 11880 KB -BaekjoonHub

--- 백준/Bronze/20362. 유니대전 퀴즈쇼/유니대전 퀴즈쇼.java
+++ 백준/Bronze/20362. 유니대전 퀴즈쇼/유니대전 퀴즈쇼.java
@@ -1,14 +1,9 @@
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.LinkedHashMap;
-import java.util.Stack;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken()), cnt = 0;
