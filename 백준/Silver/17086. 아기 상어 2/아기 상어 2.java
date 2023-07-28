fa035d4811d53dfce3fa82bbce10e50528b3fc56
Bo Ram <68523422+boram0415@users.noreply.github.com>
2023-07-28 16:35:15 +0900

    [Silver II] Title: 아기 상어 2, Time: 92 ms, Memory: 12084 KB -BaekjoonHub

--- 백준/Silver/17086. 아기 상어 2/아기 상어 2.java
+++ 백준/Silver/17086. 아기 상어 2/아기 상어 2.java
@@ -1,55 +1,51 @@
+
 import java.io.*;
 import java.util.*;
+class Main{
 
-public class Main {
-    static int[][] arr;
-    static int cnt=-1;
+    static int [][] arr;
+    static int [][] sum;
+    static Queue<int []> q = new LinkedList<>();
     static int[] disX = {-1, 1, 0, 0, -1, -1, 1, 1};
     static int[] disY = {0, 0, -1, 1, -1, 1, -1, 1};
-    static int[][] sum;
-    static Queue<int[]> q = new LinkedList<>();
-    public static void main(String[] args) throws Exception {
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[]args) throws Exception{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
+        int cnt =-1;
         arr = new int[N][M];
         sum = new int[N][M];
 
-        for (int i = 0; i < N; i++) {
+        for(int i=0;i<N;i++){
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < M; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
+            for(int j=0;j<M;j++){
+                arr[i][j]=Integer.parseInt(st.nextToken());
             }
         }
 
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                if(arr[i][j] == 1) {
-                    q.add(new int[]{i, j});
+        for(int i=0;i<N;i++){
+            for(int j=0;j<M;j++){
+                if(arr[i][j]==1){
+                    q.add(new int[]{i,j});
                 }
             }
         }
 
-        while (!q.isEmpty()) {
-            int[] n = q.poll();
-            for (int i = 0; i < 8; i++) {
-                
-                int nX = n[0] + disX[i];
-                int nY = n[1] + disY[i];
-                
-                // 인덱스 범위를 초과 시
-                if (nX < 0 || nX >= N || nY < 0 || nY >= M) continue;
-                if(sum[nX][nY]!=0 || arr[nX][nY]==1)continue;
-                
+        while(!q.isEmpty()){
+            int n [] = q.poll();
+            for(int i=0;i<8;i++){
+                int nX=n[0] + disX[i];
+                int nY=n[1] + disY[i];
+                // 인덱스 범위 넘어갔을 경우
+                if(nX < 0 || nX >= N || nY < 0 || nY >= M) continue;
+                if(arr[nX][nY] == 1 || sum[nX][nY] != 0) continue;
                 sum[nX][nY] = sum[n[0]][n[1]]+1;
-                q.offer(new int[]{nX, nY});
-                cnt = Integer.max(cnt, sum[nX][nY]);
+                cnt = Integer.max(cnt,sum[nX][nY]);
+                q.add(new int[]{nX,nY});
             }
         }
-        System.out.println(cnt);
+        System.out.print(cnt);
     }
-
-
 }
