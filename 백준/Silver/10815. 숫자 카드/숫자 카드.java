1c85391d616d26986310a1ca1fbd04bfbf14af56
Bo Ram <68523422+boram0415@users.noreply.github.com>
2023-08-19 22:50:25 +0900

    [Silver V] Title: 숫자 카드, Time: 1060 ms, Memory: 134772 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -1,56 +1,51 @@
 import java.io.*;
-import java.util.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
 public class Main {
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static int n, m;
+    static int arr[];
 
-    static int[] arr;
-
-    private static void quickSort(int[] arr, int start, int end) {
-        if (start < end) {
-            // 피봇값의 기본 자리를 찾는 메소드
-            int par = partition(arr, start, end);
-            quickSort(arr, start, par - 1);
-            quickSort(arr, par + 1, end);
-        }
-
-    }
-
-    private static int partition(int[] arr, int start, int end) {
-        int pivot = arr[end];
-        int i = start-1;
-        for (int j = start; j <= end-1; j++) {
-            if (arr[j] <= pivot) {
-                i++;
-                swap(i, j);
-            }
-        }
-        swap(i+1, end);
-        return i+1;
-    }
-
-    private static void swap(int max, int min) {
-        int tmp = arr[max];
-        arr[max] = arr[min];
-        arr[min] = tmp;
-    }
-
-    public static void main(String[] args) throws Exception{
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        int A = Integer.valueOf(br.readLine());
-        arr = new int[A];
+    public static void main(String[] args) throws IOException {
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i=0;i<A;i++){
-            arr[i] = Integer.valueOf(st.nextToken());
-        }
-        quickSort(arr,0,arr.length-1);
-        int k = Integer.valueOf(br.readLine());
+
+        for(int i = 0 ; i < n; i++)
+            arr[i] = Integer.parseInt(st.nextToken());
+
+        // 숫자 카드 오름차순 정렬
+        Arrays.sort(arr);
+
+        m = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
-        for(int i = 0 ; i < k; i++) {
+
+        for(int i = 0 ; i < m; i++) {
             int num = Integer.parseInt(st.nextToken());
-            if(Arrays.binarySearch(arr,num) > -1) sb.append("1 ");
-            else sb.append("0 ");
+            // 이분 탐색을 해서 해당 숫자가 있는 경우
+            if(binarySearch(num)) bw.write("1 ");
+            // 이분 탐색을 해서 해당 숫자가 없는 경우
+            else bw.write("0 ");
         }
-        System.out.println(sb);
+
+        bw.close();
+        br.close();
+    }
+    private static boolean binarySearch(int num) {
+        int leftIndex = 0;
+        int rightIndex = n - 1;
+
+        while(leftIndex <= rightIndex){
+            int midIndex = (leftIndex + rightIndex) / 2;
+            int mid = arr[midIndex];
+
+            if(num < mid) rightIndex = midIndex - 1;
+            else if(num > mid) leftIndex = midIndex + 1;
+            else return true;
+        }
+        return false;
     }
 }
+
