72cf38493c1284c1fab667379b3c9bc6cd590a20
Bo Ram <68523422+boram0415@users.noreply.github.com>
2023-09-14 21:23:32 +0900

    [Silver III] Title: 바이러스, Time: 76 ms, Memory: 11432 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,56 +1,48 @@
-import java.util.ArrayList;
-import java.util.Collections;
+import java.io.*;
+import java.util.*;
 
-public class Main {
-
-    static ArrayList<Integer> A[];
+class Main{
     static boolean visited[];
-    static int N;
+    static ArrayList<Integer> graph[] ;
     static int cnt;
-
-    public static void main(String[] args) throws Exception {
-
-        N = read();
-        int M = read();
-
-        A = new ArrayList[N+1];
-        for (int i = 0; i <= N; i++) {
-            A[i] = new ArrayList<>();
-        }
-
-        for (int i = 0; i < M; i++) {
-            int p = read();
-            int c = read();
-            A[p].add(c);
-            A[c].add(p);
-        }
-        for (int i = 1; i <=N; i++) {
-            Collections.sort(A[i]);
-        }
-        visited = new boolean[N + 1];
-        dfs(1);
-        System.out.println(cnt);
-    }
-
-
-    private static void dfs(int v) {
-        visited[v] = true;
-        for (int i : A[v]) {
-            if (!visited[i]) {
-                cnt++;
-                dfs(i);
-            }
-        }
-    }
-
-    static int read() throws Exception {
-        int c, n = 0;
-        while (true) {
-            c = System.in.read() - 48;
-            if (c < 0 || c > 9) return n;
+    
+    static int read() throws IOException{
+        int c=0, n=0;
+        while(true){
+            c = System.in.read()-48;
+            if(c < 0 || c > 9) return n ;
             n = n * 10 + c;
         }
     }
-
-
+   
+    public static void main(String[]args) throws IOException{
+        
+        int N = read();
+        int M = read();
+        
+        graph = new ArrayList[N+1];
+        visited = new boolean[N+1];
+        
+        for(int i=1;i<=N;i++) graph[i] = new ArrayList<>();
+        
+        for(int i=0;i<M;i++){
+            int p = read();
+            int c = read();
+            graph[p].add(c);
+            graph[c].add(p);
+        }
+        dfs(1);
+        System.out.print(cnt);
+    }
+    
+    static void dfs(int verTex){
+        visited[verTex] = true;
+        for(int node : graph[verTex]){
+            if(!visited[node]) {
+                cnt++;
+                dfs(node);
+            }
+        }
+        
+    }
 }
