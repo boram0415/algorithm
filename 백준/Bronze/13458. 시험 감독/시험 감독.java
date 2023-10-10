a6f85a59af619386078c9d2c859b73bd8ec22951
Bo Ram <68523422+boram0415@users.noreply.github.com>
2023-10-10 14:24:28 +0900

    [Bronze II] Title: 시험 감독, Time: 240 ms, Memory: 15792 KB -BaekjoonHub

--- 백준/Bronze/13458. 시험 감독/시험 감독.java
+++ 백준/Bronze/13458. 시험 감독/시험 감독.java
@@ -1,16 +1,13 @@
-import java.util.Arrays;
-
 public class Main {
-
     static int read() throws Exception {
         int c, m = 0;
+
         while (true) {
             c = System.in.read() - 48;
             if (c < 0 || c > 9) return m;
             m = m * 10 + c;
         }
     }
-
     public static void main(String[] args) throws Exception{
 
         int N = read();
@@ -19,30 +16,21 @@ public class Main {
         for (int i = 0; i < N; i++) {
             arr[i]=read();
         }
+
         int B = read();
         int C = read();
 
-        for (int i = 0; i < N; i++) {
-            // 총감독 수가 인원보다 적을 때
-            if(arr[i] > B) {
-                arr[i] -= B;
-                cnt++;
-                // 총감독 수가 인원보다 많을 때
-            }else{
-                arr[i] = 0;
-                cnt++;
-            }
-        }
-        for (int i = 0; i < N; i++) {
-            // 총 감독 감시로 모든 인원이 감시 된다면
-            if(arr[i] == 0 ) continue;
-
-            if (arr[i] % C != 0) {
-                cnt +=(arr[i]/C)+1;
-            } else {
-                cnt += (arr[i]/C);
-            }
+        for(int s : arr){
+            cnt+=1;
+            s-=B;
+            cnt += func(s,C);
         }
         System.out.println(cnt);
+    }
+
+    private static int func(int site, int c) {
+        if (site <= 0) return 0;
+        if (site%c == 0) return site/c;
+        return site/c+1;
     }
 }
