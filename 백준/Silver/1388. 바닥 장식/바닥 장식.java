9d8d69bef2da156b225796a9a2ba6aa4cbee6c74
Bo Ram <68523422+boram0415@users.noreply.github.com>
2023-10-18 21:53:37 +0900

    [Silver IV] Title: 바닥 장식, Time: 80 ms, Memory: 11620 KB -BaekjoonHub

--- 백준/Silver/1388. 바닥 장식/바닥 장식.java
+++ 백준/Silver/1388. 바닥 장식/바닥 장식.java
@@ -1,54 +1,48 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
-public class Main {
-    static int N, M;
-    static char[][] tiles;
-    static boolean[][] visited;
+class Main {
+    static char graph[][];
+    static boolean visited[][];
+    static int cnt, N, M;
+    static char flag;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String args[]) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        tiles = new char[N][M];
+        graph = new char[N][M];
         visited = new boolean[N][M];
 
         for (int i = 0; i < N; i++) {
-            String input = br.readLine();
-            for (int j = 0; j < M; j++) {
-                tiles[i][j] = input.charAt(j);
-            }
+            graph[i] = br.readLine().toCharArray();
         }
-
-        int count = 0;
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
                 if (!visited[i][j]) {
-                    dfs(i, j, tiles[i][j]);
-                    count++;
+                    dfs(graph[i][j], i, j);
+                    cnt++;
                 }
             }
+
         }
-        System.out.println(count);
+        System.out.println(cnt);
+
     }
 
-    public static void dfs(int x, int y, char type) {
-        if (x < 0 || y < 0 || x >= N || y >= M || visited[x][y] || tiles[x][y] != type) {
-            return;
+    static void dfs(char node, int x, int y) {
+        if (x <= -1 || x >= N || y <= -1 || y >= M || visited[x][y] || node != graph[x][y]) return;
+        visited[x][y] = true;
+        if (node == '-') {
+            dfs(graph[x][y], x, y + 1);
+            flag ='-';
+        }
+        if (node == '|') {
+            dfs(graph[x][y], x + 1, y);
+            flag ='|';
         }
 
-        visited[x][y] = true;
-        if (type == '-') {
-            dfs(x, y - 1, type);
-            dfs(x, y + 1, type);
-        } else { // type == '|'
-            dfs(x - 1, y, type);
-            dfs(x + 1, y, type);
-        }
     }
 }
