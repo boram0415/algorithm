3497141f8f08b4d7366322155ff65fc1c7f65402
Bo Ram <68523422+boram0415@users.noreply.github.com>
2023-10-22 21:30:47 +0900

    [Bronze V] Title: Zadanie próbne, Time: 80 ms, Memory: 11496 KB -BaekjoonHub

---
+++ 백준/Bronze/8545. Zadanie próbne/Zadanie próbne.java
@@ -0,0 +1,9 @@
+import java.util.*;
+import java.io.*;
+class Main{
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder(br.readLine());
+        System.out.print(sb.reverse());
+    }
+}
