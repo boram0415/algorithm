8294bb194de7dc6ebb4a4d07990741df14e6d458
Bo Ram <68523422+boram0415@users.noreply.github.com>
2023-10-26 23:16:16 +0900

    [Bronze IV] Title: FYI, Time: 76 ms, Memory: 11464 KB -BaekjoonHub

---
+++ 백준/Bronze/17863. FYI/FYI.java
@@ -0,0 +1,7 @@
+import java.io.*;
+class Main{
+    public static void main(String args[]) throws Exception{
+        System.out.print(new BufferedReader(new InputStreamReader(System.in)).readLine().substring(0,3).equals("555") ? "YES" : "NO");
+    }
+    
+}
