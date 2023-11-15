51410cbe229a2ad8c06aa9b5db09718c1dc294cb
Bo Ram <68523422+boram0415@users.noreply.github.com>
2023-11-15 12:31:11 +0900

    [Silver I] Title: RGB거리, Time: 76 ms, Memory: 11444 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -4,10 +4,9 @@ public class Main {
         int[][] dp = new int[n + 1][3];
 
         for (int i = 1; i <= n; i++) {
-            int r = read(),g=read(),b=read();
-            dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + r;
-            dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + g;
-            dp[i][2] = Math.min(dp[i - 1][0], dp[i - 1][1]) + b;
+            dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + read();
+            dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + read();
+            dp[i][2] = Math.min(dp[i - 1][0], dp[i - 1][1]) + read();
         }
         System.out.println(Math.min(dp[n][0], Math.min(dp[n][1], dp[n][2])));
     }
