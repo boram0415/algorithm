d37e5f213694f62ea5aa90ebc1629e97e1a134fd
Bo Ram <68523422+boram0415@users.noreply.github.com>
2023-11-17 16:42:37 +0900

    [Silver II] Title: 좌표 압축, Time: 1716 ms, Memory: 166584 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.java
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -1,52 +1,50 @@
-import java.io.*;
 import java.util.*;
+
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        int[] nums = new int[Integer.parseInt(br.readLine())];
-        int w =0;
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        
-        // 자연수 초기화
-        while (st.hasMoreTokens()) {
-            nums[w++] = Integer.parseInt(st.nextToken());
+    static StringBuilder sb = new StringBuilder(100);
+    static Set<Integer> set = new TreeSet<>();
+    static int cnt = 0;
+    static int zip[];
+
+    public static void main(String[] args) throws Exception {
+
+        int arr[] = new int[read()];
+
+        for (int i = 0; i < arr.length; i++) {
+            int num = read();
+            arr[i] = num;
+            set.add(num);
         }
-        // TreeSet을 이용한 중복제거와 정렬
-        Set<Integer> set = new TreeSet<>();
-        for (int i = 0; i < nums.length; i++) {
-            set.add(nums[i]);
-        }
-        // 중복제거 정렬된 값 담기
-        int[] arr = new int[set.size()];
+
+        zip = new int[set.size()];
         Iterator i = set.iterator();
-        int j = 0;
-        while (i.hasNext()) {
-            arr[j++] = Integer.parseInt(i.next().toString());
-        }
-        //이진탐색
-        for (int x = 0; x < nums.length; x++) {
-            int find = nums[x];
-            int mid ;
-            int left =0 , right =arr.length-1;
-            while (left <= right) {
+        while (i.hasNext()) zip[cnt++] = Integer.parseInt(i.next().toString());
 
-                mid= (right+left)/2;
-
-                if (find == arr[mid]) {
-                    sb.append(mid + " ");
-                }
-
-                if (find < arr[mid]) {
-                    right = mid -1;
-                }else{
-                    left = mid +1;
-                }
-
-            }
-        }
+        for (int h = 0; h < arr.length; h++)
+            sb.append(binarySearch(arr[h], 0, zip.length - 1)).append(" ");
 
         System.out.println(sb);
+    }
+
+    private static int binarySearch(int findNum, int s, int e) {
+
+        // 중간값
+        int mid = (s + e) / 2;
+        // 찾기
+        if (findNum == zip[mid]) return mid;
+        // 비교
+        if (zip[mid] > findNum) return binarySearch(findNum, s, mid - 1);
+        else return binarySearch(findNum, mid + 1, e);
+
+    }
+
+    private static int read() throws Exception {
+
+        int c, n = System.in.read() & 15;
+        boolean isNegative = n == 13;
+        if (isNegative) n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+        return isNegative ? ~n + 1 : n;
 
     }
 }
