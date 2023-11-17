ef57c784403999ebe8fed79aa2d45db5b22a9bce
Bo Ram <68523422+boram0415@users.noreply.github.com>
2023-11-17 21:10:21 +0900

    [Silver I] Title: IOIOI, Time: 184 ms, Memory: 20084 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.java
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -2,35 +2,25 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Main {
-
-    public static void main(String[] args) throws Exception{
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        int N = Integer.parseInt(br.readLine());
-        int M = Integer.parseInt(br.readLine());
-        String S = br.readLine();
-        int cnt=0;
-        sb.append("I");
-        for (int i = 0; i < N; i++) {
-            sb.append("OI");
-        }
-        String cp = sb.toString();
-        int p = N+N+1;
-        for (int i = 0; i < S.length()-1; i++) {
-            String tmp = S.substring(i,i+p);
-            if(i+p > S.length()-1){
-                if (cp.equals(tmp)) {
+        public static void main(String[] args) throws Exception {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            int N = Integer.parseInt(br.readLine());
+            int M = Integer.parseInt(br.readLine());
+            String str = br.readLine();
+            int sum = 0,cnt=0;
+            for (int i = 1; i <M-1; i++) {
+                if (str.charAt(i-1) == 'I' && str.charAt(i) == 'O' && str.charAt(i+1) == 'I') {
                     cnt++;
-                }
-                break;
-            }
-            if(S.charAt(i) == 'I'){
-                if (cp.equals(tmp)) {
-                    cnt++;
+                    if(cnt == N) {
+                        cnt--;
+                        sum++;
+                    }
+                    i++;
+                }else{
+                    cnt=0;
                 }
             }
-        }
-        System.out.println(cnt);
-    }
-}
+            System.out.println(sum);
+
+        } 
+    } 
