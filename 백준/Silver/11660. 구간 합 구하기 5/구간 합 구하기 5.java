27e3ecb005966c591742b8bd5c309d8b5759a4d8
Bo Ram <68523422+boram0415@users.noreply.github.com>
2023-11-23 17:57:16 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 640 ms, Memory: 23620 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -1,33 +1,39 @@
 import java.io.*;
 import java.util.*;
+
 class Main {
-    static StringTokenizer st;
+
     public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+        StringBuilder sb = new StringBuilder();
+        int N = read();
+        int M = read();
         int[][] arr = new int[N][N + 1];
 
         for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 1; j < N+1; j++) {
-                arr[i][j] = arr[i][j-1] + Integer.parseInt(st.nextToken());
+            for (int j = 1; j < N + 1; j++) {
+                arr[i][j] = arr[i][j - 1] + read();
             }
         }
-        StringBuilder sb = new StringBuilder();
-        int sum=0;
+        
+        int sum = 0;
         while (M-- > 0) {
-            st = new StringTokenizer(br.readLine());
-            int x1 = Integer.parseInt(st.nextToken())-1, x2 = Integer.parseInt(st.nextToken()),
-                    y1 = Integer.parseInt(st.nextToken())-1, y2 = Integer.parseInt(st.nextToken());
+            int x1 = read() - 1, x2 = read(), y1 = read() - 1, y2 = read();
             for (int i = x1; i <= y1; i++) {
-                sum += arr[i][y2]-arr[i][x2-1] ;
+                sum += arr[i][y2] - arr[i][x2 - 1];
             }
             sb.append(sum).append("\n");
-            sum=0;
+            sum = 0;
         }
         System.out.print(sb);
 
+    }
+
+    static int read() throws Exception {
+        int c = 0, n = 0;
+        while (true) {
+            c = System.in.read() - 48;
+            if (c < 0 || c > 9) return n;
+            n = n * 10 + c;
+        }
     }
 }
