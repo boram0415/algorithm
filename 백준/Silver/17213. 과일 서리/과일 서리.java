85f5b76b7ae76a77313580b4282c6028c0823b97
Bo Ram <68523422+boram0415@users.noreply.github.com>
2024-04-03 19:49:45 +0900

    [Silver II] Title: 과일 서리, Time: 80 ms, Memory: 11464 KB -BaekjoonHub

---
+++ 백준/Silver/17213. 과일 서리/과일 서리.java
@@ -0,0 +1,21 @@
+import java.io.*;
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+    int m = Integer.parseInt(br.readLine());
+    int[][] dp = new int[11][31];
+    for (int i = 1; i <= m; i++) {
+      dp[1][i] = 1;
+    }
+    for (int i = 1; i <= n; i++) {
+      dp[i][i] = 1;
+    }
+    for (int i = 2; i <= n; i++) {
+      for (int j = i; j <= m; j++) {
+        dp[i][j] = dp[i][j-1] + dp[i-1][j-1];
+      }
+    }
+    System.out.print(dp[n][m]);
+  }
+}
