01f7cddd17cf8f830c518bb44604f4534d8351c2
Bo Ram <68523422+boram0415@users.noreply.github.com>
2024-07-25 18:39:52 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.71 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -1,21 +1,13 @@
 class Solution {
     public String solution(String s) {
-        StringBuilder result = new StringBuilder();
-        boolean newWord = true;  // 새로운 단어의 시작을 추적
+        String[] arr = s.toLowerCase().split("");
+        boolean check = true;
+        StringBuilder answer = new StringBuilder();
 
-        for (char c : s.toCharArray()) {
-            if (c == ' ') {
-                result.append(c);
-                newWord = true;  // 공백 다음은 새로운 단어의 시작
-            } else {
-                if (newWord) {
-                    result.append(Character.toUpperCase(c));  // 새로운 단어의 첫 글자는 대문자
-                    newWord = false;
-                } else {
-                    result.append(Character.toLowerCase(c));  
-                }
-            }
+        for(String one : arr){
+            answer.append(check ? one.toUpperCase() : one);
+            check = one.equals(" ") ? true : false;
         }
-        return result.toString();
+        return answer.toString();
     }
 }
