e3eb2ec30e136025da0975a90ace6ef43bc43032
Bo Ram <68523422+boram0415@users.noreply.github.com>
2024-07-29 12:53:28 +0900

    [level 2] Title: 기능개발, Time: 3.59 ms, Memory: 80.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Solution {
+    static int N = 100;
+   public int[] solution(int[] progresses, int[] speeds) {
+        List<Integer> list = new LinkedList<>();
+        list.add(1);
+        int tmp = 0,max=-1;
+        for (int i = 0; i < progresses.length-1; i++) {
+            // 나눠지지 않을 경우 +1day
+            int a = (int)Math.ceil((N - progresses[i]) / (double) speeds[i]);
+            int b = (int)Math.ceil((N - progresses[i+1]) / (double) speeds[i+1]);
+            // 앞에 있는 작업 소요 시간이 이후 오는 모든 작업의 소요시간보다 크면 같이 배포함
+            max = Math.max(a, max);
+            if (max >= b) {
+                list.set(tmp, list.get(tmp) + 1);
+                continue;
+            }
+            list.add(1);
+            tmp++;
+        }
+
+        int[] answer = list.stream().mapToInt(i -> i).toArray();
+        return answer;
+    }
+}
