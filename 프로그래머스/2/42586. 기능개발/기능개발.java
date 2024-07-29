1b2102d894b6f15b1e84161c2bb353da94992f08
Bo Ram <68523422+boram0415@users.noreply.github.com>
2024-07-29 20:25:21 +0900

    [level 2] Title: 기능개발, Time: 2.92 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -2,13 +2,13 @@ import java.util.*;
 class Solution {
     static int N = 100;
    public int[] solution(int[] progresses, int[] speeds) {
-        List<Integer> list = new LinkedList<>();
+       List<Integer> list = new ArrayList<>();
         list.add(1);
-        int tmp = 0,max=-1;
-        for (int i = 0; i < progresses.length-1; i++) {
+        int tmp = 0, max = -1;
+        for (int i = 0; i < progresses.length - 1; i++) {
             // 나눠지지 않을 경우 +1day
-            int a = (int)Math.ceil((N - progresses[i]) / (double) speeds[i]);
-            int b = (int)Math.ceil((N - progresses[i+1]) / (double) speeds[i+1]);
+            int a = (int) Math.ceil((N - progresses[i]) / (double) speeds[i]);
+            int b = (int) Math.ceil((N - progresses[i + 1]) / (double) speeds[i + 1]);
             // 앞에 있는 작업 소요 시간이 이후 오는 모든 작업의 소요시간보다 크면 같이 배포함
             max = Math.max(a, max);
             if (max >= b) {
@@ -18,8 +18,6 @@ class Solution {
             list.add(1);
             tmp++;
         }
-
-        int[] answer = list.stream().mapToInt(i -> i).toArray();
-        return answer;
+        return list.stream().mapToInt(i -> i).toArray();
     }
 }
