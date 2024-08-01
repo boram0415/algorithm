29c8aa79e20290cef6f415e7996161cfa4ba5dfe
Bo Ram <68523422+boram0415@users.noreply.github.com>
2024-08-02 00:02:41 +0900

    [level 1] Title: 카드 뭉치, Time: 0.04 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/README.md
+++ 프로그래머스/1/159994. 카드 뭉치/README.md
@@ -1,6 +1,6 @@
 # [level 1] 카드 뭉치 - 159994 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/159994?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/159994) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 02일 00:02:30
+2024년 08월 02일 00:02:39
 
 ### 문제 설명
 
--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -1,33 +1,34 @@
 public class Solution {
     public String solution(String[] cards1, String[] cards2, String[] goal) {
-        // cards1과 cards2에서 현재 검사할 인덱스
-        int i1 = 0, i2 = 0;
-
-        for (String g : goal) {
+        // 각 카드 뭉치에서 현재 검사할 인덱스를 저장할 변수
+        int index1 = 0, index2 = 0;
+        // 목표 배열을 순회하며 각 단어를 확인
+        for (String word : goal) {
             boolean found = false;
-
-            // cards1에서 현재 goal 단어를 찾기
-            while (i1 < cards1.length && cards1[i1].equals(g)) {
-                i1++;
-                found = true;
-                break;
-            }
-
-            // cards1에서 못 찾았고 cards2에서 현재 goal 단어를 찾기
-            if (!found) {
-                while (i2 < cards2.length && cards2[i2].equals(g)) {
-                    i2++;
-                    found = true;
-                    break;
+            // 현재 단어가 cards1에 있고 해당 인덱스가 아직 사용되지 않았다면
+            while (index1 < cards1.length && !found) {
+                if (cards1[index1].equals(word)) {
+                    found = true; // 단어를 찾았으므로 반복 종료
+                    index1++; // 다음 카드로 이동
+                } else {
+                    index1++; // 찾지 못했다면 다음 카드로 이동
                 }
             }
-
-            // 만약 두 배열에서도 단어를 찾지 못한 경우
+            // cards1에서 못 찾았을 경우 cards2에서 찾기
+            while (!found && index2 < cards2.length) {
+                if (cards2[index2].equals(word)) {
+                    found = true; // 단어를 찾았으므로 반복 종료
+                    index2++; // 다음 카드로 이동
+                } else {
+                    index2++; // 찾지 못했다면 다음 카드로 이동
+                }
+            }
+            // 만약 두 배열에서도 단어를 찾지 못했다면 목표 단어 배열을 만들 수 없음
             if (!found) {
                 return "No";
             }
         }
-
+        // 모든 단어를 찾았다면 "Yes" 반환
         return "Yes";
     }
 }
