4f4a1fe14357e87927024fa6315f47082e648790
Bo Ram <68523422+boram0415@users.noreply.github.com>
2024-08-04 00:52:31 +0900

    [Silver V] Title: 숫자 카드, Time: 924 ms, Memory: 141684 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -3,49 +3,50 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static int n, m;
-    static int arr[];
+    static int[] arr;
 
-    public static void main(String[] args) throws IOException {
-        n = Integer.parseInt(br.readLine());
-        arr = new int[n];
-        StringTokenizer st = new StringTokenizer(br.readLine());
+    public static void main(String[] args) {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
+            
+            int n = Integer.parseInt(br.readLine());
+            arr = new int[n];
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(st.nextToken());
+            }
+            Arrays.sort(arr);
 
-        for(int i = 0 ; i < n; i++)
-            arr[i] = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(br.readLine());
+            st = new StringTokenizer(br.readLine());
+            StringBuilder sb = new StringBuilder();
 
-        // 숫자 카드 오름차순 정렬
-        Arrays.sort(arr);
+            for (int i = 0; i < m; i++) {
+                int num = Integer.parseInt(st.nextToken());
+                sb.append(binarySearch(num) ? "1 " : "0 ");
+            }
 
-        m = Integer.parseInt(br.readLine());
-        st = new StringTokenizer(br.readLine());
-
-        for(int i = 0 ; i < m; i++) {
-            int num = Integer.parseInt(st.nextToken());
-            // 이분 탐색을 해서 해당 숫자가 있는 경우
-            if(binarySearch(num)) bw.write("1 ");
-            // 이분 탐색을 해서 해당 숫자가 없는 경우
-            else bw.write("0 ");
+            bw.write(sb.toString());
+            bw.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
-        bw.close();
-        br.close();
     }
+
     private static boolean binarySearch(int num) {
         int leftIndex = 0;
-        int rightIndex = n - 1;
+        int rightIndex = arr.length - 1;
 
-        while(leftIndex <= rightIndex){
+        while (leftIndex <= rightIndex) {
             int midIndex = (leftIndex + rightIndex) / 2;
-            int mid = arr[midIndex];
-
-            if(num < mid) rightIndex = midIndex - 1;
-            else if(num > mid) leftIndex = midIndex + 1;
-            else return true;
+            if (num < arr[midIndex]) {
+                rightIndex = midIndex - 1;
+            } else if (num > arr[midIndex]) {
+                leftIndex = midIndex + 1;
+            } else {
+                return true;
+            }
         }
         return false;
     }
 }
-
