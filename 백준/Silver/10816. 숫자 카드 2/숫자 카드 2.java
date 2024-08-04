64dc621d7c042137c1cfd71a6612bf9194dfc2a5
Bo Ram <68523422+boram0415@users.noreply.github.com>
2024-08-05 01:08:21 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 736 ms, Memory: 253120 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -1,25 +1,38 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
+
 public class Main {
-	public static void main ( String [] args ) throws IOException {
+    static final int MAX_RANGE = 20000000;
+    static final int OFFSET = 10000000;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] frequency = readFrequencies(br, n);
+
+        int m = Integer.parseInt(br.readLine());
+        StringBuilder result = generateResults(br, m, frequency);
         
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int [] arr = new int [20000001];
-		int N = Integer.parseInt(br.readLine());
-		StringTokenizer st = new StringTokenizer(br.readLine());
-        
-		for(int i = 0; i<N; i++) {
-			int num = Integer.parseInt(st.nextToken());
-			arr[num+10000000]++;
-		}
-		int M = Integer.parseInt(br.readLine());
-		
-		st = new StringTokenizer(br.readLine());
-		StringBuilder sb = new StringBuilder();
-		
-		for(int i = 0; i < M; i++) {
-			sb.append(arr[Integer.parseInt(st.nextToken()) + 10000000]).append(' ');
-		}
-		System.out.println(sb);
-	}
+        System.out.println(result.toString());
+    }
+
+    private static int[] readFrequencies(BufferedReader br, int count) throws IOException {
+        int[] frequency = new int[MAX_RANGE + 1];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < count; i++) {
+            int number = Integer.parseInt(st.nextToken()) + OFFSET;
+            frequency[number]++;
+        }
+        return frequency;
+    }
+
+    private static StringBuilder generateResults(BufferedReader br, int count, int[] frequency) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < count; i++) {
+            int query = Integer.parseInt(st.nextToken()) + OFFSET;
+            sb.append(frequency[query]).append(" ");
+        }
+        return sb;
+    }
 }
