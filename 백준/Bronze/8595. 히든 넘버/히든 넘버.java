f68c038289ee3564bdcfc5756d265cd0bed8abad
Bo Ram <68523422+boram0415@users.noreply.github.com>
2024-08-18 00:03:08 +0900

    [Bronze I] Title: 히든 넘버, Time: 196 ms, Memory: 56352 KB -BaekjoonHub

--- 백준/Bronze/8595. 히든 넘버/히든 넘버.java
+++ 백준/Bronze/8595. 히든 넘버/히든 넘버.java
@@ -1,14 +1,33 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		br.readLine();
-		long ans=0;
-		StringTokenizer st = new StringTokenizer(br.readLine().replaceAll("[a-zA-Z]", " "));
-		while(st.hasMoreTokens())
-			ans+=Integer.parseInt(st.nextToken());
-		System.out.println(ans);
-	}
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        br.readLine(); // 첫 번째 줄은 사용하지 않음
+        long ans = 0;
+        String input = br.readLine();
+
+        int length = input.length();
+        int number = 0;
+        boolean isNumber = false;
+
+        for (int i = 0; i < length; i++) {
+            char c = input.charAt(i);
+
+            if (c >= '0' && c <= '9') { // 숫자 문자만 처리
+                number = number * 10 + (c - '0');
+                isNumber = true;
+            } else if (isNumber) {
+                ans += number;
+                number = 0;
+                isNumber = false;
+            }
+        }
+
+        if (isNumber) {
+            ans += number; // 마지막에 남은 숫자 처리
+        }
+
+        System.out.println(ans);
+    }
 }
